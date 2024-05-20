365bc2e52a6f344b314a7efd291c6b9bdc59687d
wyndale <enchanteurtsukuba88@gmail.com>
2024-05-19 20:30:53 -0800

    Fourth modification, fix bugs, added bank system

---
+++ BankSystem.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BankSystem
+{
+    public class CustomerType
+    {
+        public enum Customer_Type
+        {
+            Individual,
+            Company
+        }
+
+        public Customer_Type type { get; set; }
+
+        public CustomerType(Customer_Type type)
+        {
+            this.type = type;
+        }
+    }
+
+    public interface ICustomerBankDatabase
+    {
+        void AddCustomer(Customer customer);
+        Customer FindCustomer(string fullname, int password);
+        IEnumerable<Customer> GetCustomers();
+        void SaveCustomers();
+    }
+
+    public class BankCustomerDatabase : ICustomerBankDatabase
+    {
+        private List<Customer> customers = new List<Customer>();
+        private readonly string _filepath = "bankdatabase.json";
+
+        public BankCustomerDatabase()
+        {
+            LoadCustomers();
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            SaveCustomers();
+        }
+
+        public Customer FindCustomer(string fullname, int password)
+        {
+            return customers.FirstOrDefault(c => c.FullName == fullname && c.Password == password);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return customers;
+        }
+
+        public void SaveCustomers()
+        {
+            string json = JsonConvert.SerializeObject(customers, Formatting.Indented);
+            File.WriteAllText(_filepath, json);
+        }
+
+        public void LoadCustomers()
+        {
+            if (File.Exists(_filepath))
+            {
+                string json = File.ReadAllText(_filepath);
+                customers = JsonConvert.DeserializeObject<List<Customer>>(json) ?? new List<Customer>();
+            }
+        }
+    }
+
+    public class Customer
+    {
+        private string fullname;
+        private string email;
+        private int password;
+        private CustomerType.Customer_Type type;
+        private decimal balance;
+        private List<Transaction> transactions;
+
+        public string FullName { get => fullname; set => fullname = value; }
+        public string Email { get => email; set => email = value; }
+        public int Password { get => password; set => password = value; }
+        public CustomerType.Customer_Type Type { get => type; set => type = value; }
+        public decimal Balance { get => balance; set => balance = value; }
+        public List<Transaction> Transactions { get => transactions; set => transactions = value; }
+
+        public Customer(string fullname, string email, int password, CustomerType.Customer_Type type, decimal balance)
+        {
+            FullName = fullname;
+            Email = email;
+            Password = password;
+            Type = type;
+            Balance = balance;
+            Transactions = new List<Transaction>();
+        }
+    }
+
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+
+        public Transaction(string type, decimal amount)
+        {
+            Date = DateTime.Now;
+            Type = type;
+            Amount = amount;
+        }
+    }
+
+    public class Bank
+    {
+        private readonly ICustomerBankDatabase _database;
+
+        public ICustomerBankDatabase CustomerDatabase => _database;
+
+        public Bank(ICustomerBankDatabase database)
+        {
+            _database = database;
+        }
+
+        public void AddCustomer(string fullname, string email, int password, CustomerType.Customer_Type type, decimal balance)
+        {
+            var customer = new Customer(fullname, email, password, type, balance);
+            CustomerDatabase.AddCustomer(customer);
+        }
+
+        public Customer FindCustomer(string fullname, int password)
+        {
+            return CustomerDatabase.FindCustomer(fullname, password);
+        }
+
+        public string MaskEmail(string email)
+        {
+            var atIndex = email.IndexOf('@');
+            if (atIndex > 3)
+            {
+                return email.Substring(0, 3) + new string('*', atIndex - 3) + email.Substring(atIndex);
+            }
+            return new string('*', atIndex) + email.Substring(atIndex);
+        }
+
+        public void DisplayEmail(string fullname, int password)
+        {
+            var customer = _database.FindCustomer(fullname, password);
+            if (customer != null)
+            {
+                Console.WriteLine("\n");
+                string maskedEmail = MaskEmail(customer.Email);
+                Console.WriteLine($"Account type: {customer.Type}");
+                Console.WriteLine($"Full name: {customer.FullName}");
+                Console.WriteLine($"Email: {maskedEmail}");
+            }
+        }
+    }
+
+    public class DepositAccount : Bank
+    {
+        public DepositAccount(ICustomerBankDatabase database) : base(database)
+        { }
+
+        public void Deposit(Customer customer, decimal amount)
+        {
+            customer.Balance += amount;
+            customer.Transactions.Add(new Transaction("Deposit", amount));
+            CustomerDatabase.SaveCustomers();
+            Console.WriteLine("\n\n________________________________________\n");
+            Console.WriteLine($"\nDeposit successfully!\n\nNew balance: {customer.Balance}");
+            Console.Write("\n________________________________________\n");
+        }
+
+        public void Withdraw(Customer customer, decimal amount)
+        {
+            if (customer.Balance >= amount)
+            {
+                customer.Balance -= amount;
+                customer.Transactions.Add(new Transaction("Withdrawal", amount));
+                CustomerDatabase.SaveCustomers();
+                Console.WriteLine("\n\n________________________________________\n");
+                Console.WriteLine($"\nWithdraw successfully!\n\nNew balance: {customer.Balance}");
+                Console.Write("\n________________________________________\n");
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+        }
+    }
+
+    public class LoanAccount : Bank
+    {
+        private const decimal InterestRateIndividual = 0.05m;
+        private const decimal InterestRateCompany = 0.04m;
+
+        public LoanAccount(ICustomerBankDatabase database) : base(database)
+        { }
+
+        public void ApplyForLoan(Customer customer, decimal amount)
+        {
+            customer.Balance += amount;
+            customer.Transactions.Add(new Transaction("Loan", amount));
+            CustomerDatabase.SaveCustomers();
+            Console.WriteLine("\n\n________________________________________\n");
+            Console.WriteLine($"\nApply for loan successful!");
+            Console.Write("\n________________________________________\n");
+        }
+
+        public void MakeLoanPayment(Customer customer, decimal amount)
+        {
+            decimal interestRate = customer.Type == CustomerType.Customer_Type.Individual ? InterestRateIndividual : InterestRateCompany;
+            decimal interest = amount * interestRate;
+            customer.Balance -= (amount + interest);
+            customer.Transactions.Add(new Transaction("Loan Payment", amount));
+            CustomerDatabase.SaveCustomers();
+            Console.WriteLine("\n\n________________________________________\n");
+            Console.WriteLine($"\nLoan payment successful!");
+            Console.Write("\n________________________________________\n");
+        }
+    }
+
+    public class MortgageAccount : Bank
+    {
+        private const decimal InterestRateIndividual = 0.04m;
+        private const decimal InterestRateCompany = 0.03m;
+
+        public MortgageAccount(ICustomerBankDatabase database) : base(database)
+        { }
+
+        public void ApplyForMortgage(Customer customer, decimal amount)
+        {
+            customer.Balance += amount;
+            customer.Transactions.Add(new Transaction("Mortgage", amount));
+            CustomerDatabase.SaveCustomers();
+            Console.WriteLine("\n\n________________________________________\n");
+            Console.WriteLine($"\nApply for mortgage loan successful!");
+            Console.Write("\n________________________________________\n");
+        }
+
+        public void MakeMortgagePayment(Customer customer, decimal amount)
+        {
+            decimal interestRate = customer.Type == CustomerType.Customer_Type.Individual ? InterestRateIndividual : InterestRateCompany;
+            decimal interest = amount * interestRate;
+            customer.Balance -= (amount + interest);
+            customer.Transactions.Add(new Transaction("Mortgage Payment", amount));
+            CustomerDatabase.SaveCustomers();
+            Console.WriteLine("\n\n________________________________________\n");
+            Console.WriteLine($"\nPayment for mortgage loan successful!");
+            Console.Write("\n________________________________________\n");
+        }
+    }
+
+    public class BankTransactionHistory : Bank
+    {
+        public BankTransactionHistory(ICustomerBankDatabase database) : base(database)
+        { }
+
+        public void DisplayHistory(string fullname, int password)
+        {
+            var customer = FindCustomer(fullname, password);
+            if (customer != null)
+            {
+                Console.WriteLine($"\n\nTransaction history of {fullname}\n");
+                foreach (var transaction in customer.Transactions)
+                {
+                    Console.WriteLine($"{transaction.Date}: {transaction.Type} of {transaction.Amount:C}");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using TellerMachine;
-using Newtonsoft.Json;
+using BankSystem;
 
 class Program
 {
@@ -17,56 +17,150 @@ class Program
         atm.AddCustomer("Wendel Lapura", "lapurawendel95@gmail.com", 12345678, 2004, new Card(Card.CardType.Debit), 1000000.00m);
         atm.AddCustomer("Jane Smith", "jane.smith@example.com", 98765, 5678, new Card(Card.CardType.ATM), 50000.00m);
 
+        BankCustomerDatabase bankdatabase = new BankCustomerDatabase();
+
+        Bank bank = new Bank(bankdatabase);
+        DepositAccount depositAccount = new DepositAccount(bankdatabase);
+        LoanAccount loanAccount = new LoanAccount(bankdatabase);
+        MortgageAccount mortgageAccount = new MortgageAccount(bankdatabase);
+        BankTransactionHistory bankHistory = new BankTransactionHistory(bankdatabase);
+
+        bank.AddCustomer("Wyndale Ocero", "wendellapura@gmail.com", 082004, CustomerType.Customer_Type.Individual, 1000000000m);
+        bank.AddCustomer("Wendel Tabid", "enchanteurtsukuba88@gmail.com", 200408, CustomerType.Customer_Type.Company, 9999999999m);
+
         bool operations = true;
+        bool continueSession = true;
 
-            try
+        while (continueSession)
+        {
+
+            Console.WriteLine("Select operation:\n\n1. Bank Operation\n2. Automated Teller Machine\n3. Exit\n\n");
+            Console.Write("> ");
+            int inputOperation = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            switch (inputOperation)
             {
-                Console.Write("Enter account number\n\n");
-                Console.Write("> ");
-                ulong accountNumber = ulong.Parse(Console.ReadLine());
+                case 1:
+                    Bank();
+                    break;
+                case 2:
+                    Teller();
+                    break;
+                case 3:
+                    Console.WriteLine("\nExiting...\n");
+                    return;
+                default:
+                    Console.WriteLine("Invalid number of operation!");
+                    break;
+            }
 
-                var customer = customerDatabase.FindCustomer(accountNumber);
-
-                if (customer != null)
+            void Bank()
+            {
+                try
                 {
-                    Console.Write("\nEnter pin code\n\n> ");
-                    int pinCode = int.Parse(Console.ReadLine());
+                    Console.WriteLine("\nBANK SYSTEM");
+                    Console.Write("\nEnter full name\n\n> ");
+                    string fullname = Console.ReadLine();
+                    Console.Write("\nEnter password\n\n> ");
+                    int password = int.Parse(Console.ReadLine());
 
-                    if (customer.PinCode == pinCode)
+                    var bankCustomer = bankdatabase.FindCustomer(fullname, password);
+
+                    if (bankCustomer != null && bankCustomer.Password == password)
                     {
+                        bank.DisplayEmail(fullname, password);
                         while (operations)
                         {
-                            atm.DisplayMaskInfo(accountNumber);
-
-                            Console.WriteLine("\n\nSelect operation:\n\n 1. Withdraw 2. Check Balance 3. Transfer Money 4. Transaction History\n\n");
+                            Console.WriteLine("\n\nSelect bank operation:\n\n1. Deposit\n2. Loan\n3. Mortgage\n4. Transaction History\n5. Exit\n");
                             Console.Write("> ");
-                            int operation = int.Parse(Console.ReadLine());
+                            int bank_operation = int.Parse(Console.ReadLine());
 
-                            switch (operation)
+                            switch (bank_operation)
                             {
                                 case 1:
-                                    Console.Write("\n\nEnter amount to withdraw: ");
-                                    decimal amount = decimal.Parse(Console.ReadLine());
-                                    withdraw.withdrawalReceipt += HandleWithdrawalReceipt;
-                                    withdraw.CashWithdrawal(amount, accountNumber);
-                                    withdraw.withdrawalReceipt -= HandleWithdrawalReceipt;
+                                    Console.WriteLine("\nChoose an option:");
+                                    Console.WriteLine("1. Deposit");
+                                    Console.WriteLine("2. Withdraw");
+                                    Console.Write("\n> ");
+                                    int depositOption = int.Parse(Console.ReadLine());
+                                    if (depositOption == 1)
+                                    {
+                                        Console.Write("\nEnter amount to deposit: ");
+                                        decimal depositAmount = decimal.Parse(Console.ReadLine());
+                                        depositAccount.Deposit(bankCustomer, depositAmount);
+                                    }
+                                    else if (depositOption == 2)
+                                    {
+                                        Console.Write("\nEnter amount to withdraw: ");
+                                        decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+                                        try
+                                        {
+                                            depositAccount.Withdraw(bankCustomer, withdrawAmount);
+                                        }
+                                        catch (InvalidOperationException ex)
+                                        {
+                                            Console.WriteLine(ex.Message);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Invalid option.");
+                                    }
                                     break;
                                 case 2:
-                                    checkBalance.AccountBalance(accountNumber);
+                                    Console.WriteLine("\nChoose an option:");
+                                    Console.WriteLine("1. Apply for Loan");
+                                    Console.WriteLine("2. Make Loan Payment");
+                                    Console.Write("\n> ");
+                                    int loanOption = int.Parse(Console.ReadLine());
+                                    if (loanOption == 1)
+                                    {
+                                        Console.Write("\nEnter amount to loan: ");
+                                        decimal loanAmount = decimal.Parse(Console.ReadLine());
+                                        loanAccount.ApplyForLoan(bankCustomer, loanAmount);
+                                    }
+                                    else if (loanOption == 2)
+                                    {
+                                        Console.Write("\nEnter amount to pay for loan: ");
+                                        decimal loanPaymentAmount = decimal.Parse(Console.ReadLine());
+                                        loanAccount.MakeLoanPayment(bankCustomer, loanPaymentAmount);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Invalid option.");
+                                    }
                                     break;
                                 case 3:
-                                    Console.Write("\nEnter the destination account number: ");
-                                    ulong toAccount = ulong.Parse(Console.ReadLine());
-                                    atm.DisplayMaskInfo(toAccount);
-                                    Console.Write("\nEnter amount to transfer: ");
-                                    decimal transferAmount = decimal.Parse(Console.ReadLine());
-                                    transfer.Transfer(accountNumber, toAccount, transferAmount);
+                                    Console.WriteLine("\nChoose an option:");
+                                    Console.WriteLine("1. Apply for Mortgage");
+                                    Console.WriteLine("2. Make Mortgage Payment");
+                                    Console.Write("\n> ");
+                                    int mortgageOption = int.Parse(Console.ReadLine());
+                                    if (mortgageOption == 1)
+                                    {
+                                        Console.Write("\nEnter amount to mortgage: ");
+                                        decimal mortgageAmount = decimal.Parse(Console.ReadLine());
+                                        mortgageAccount.ApplyForMortgage(bankCustomer, mortgageAmount);
+                                    }
+                                    else if (mortgageOption == 2)
+                                    {
+                                        Console.Write("\nEnter amount to pay for mortgage: ");
+                                        decimal mortgagePaymentAmount = decimal.Parse(Console.ReadLine());
+                                        mortgageAccount.MakeMortgagePayment(bankCustomer, mortgagePaymentAmount);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Invalid option.");
+                                    }
                                     break;
                                 case 4:
-                                    history.DisplayHistory(accountNumber);
+                                    bankHistory.DisplayHistory(fullname, password);
                                     break;
+                                case 5:
+                                    Console.WriteLine("\nExiting...\n");
+                                    return;
                                 default:
-                                    Console.WriteLine("\nInvalid operation selected.");
+                                    Console.WriteLine("Invalid operation!");
                                     break;
                             }
 
@@ -86,20 +180,105 @@ class Program
                     }
                     else
                     {
-                        Console.WriteLine("\nIncorrect pin code!");
+                        throw new Exception("Invalid name or password");
                     }
                 }
-                else
+                catch (Exception e)
                 {
-                    Console.WriteLine("\nAccount number not found!");
+                    Console.WriteLine("\n");
+                    Console.WriteLine(e.Message);
                 }
+                Console.WriteLine();
             }
-            catch
-            {
-                Console.WriteLine($"\nSomething went wrong!\n");
-            }
-            Console.WriteLine();
 
+            void Teller()
+            {
+                try
+                {
+                    Console.WriteLine("\nAUTOMATED TELLER MACHINE");
+                    Console.Write("\n\nEnter account number\n\n");
+                    Console.Write("> ");
+                    ulong accountNumber = ulong.Parse(Console.ReadLine());
+
+                    var customer = customerDatabase.FindCustomer(accountNumber);
+
+                    if (customer != null)
+                    {
+                        Console.Write("\nEnter pin code\n\n> ");
+                        int pinCode = int.Parse(Console.ReadLine());
+
+                        if (customer.PinCode == pinCode)
+                        {
+                            while (operations)
+                            {
+                                atm.DisplayMaskInfo(accountNumber);
+
+                                Console.WriteLine("\n\nSelect operation:\n\n 1. Withdraw 2. Check Balance 3. Transfer Money 4. Transaction History 5. Exit\n\n");
+                                Console.Write("> ");
+                                int operation = int.Parse(Console.ReadLine());
+
+                                switch (operation)
+                                {
+                                    case 1:
+                                        Console.Write("\n\nEnter amount to withdraw: ");
+                                        decimal amount = decimal.Parse(Console.ReadLine());
+                                        withdraw.withdrawalReceipt += HandleWithdrawalReceipt;
+                                        withdraw.CashWithdrawal(amount, accountNumber);
+                                        withdraw.withdrawalReceipt -= HandleWithdrawalReceipt;
+                                        break;
+                                    case 2:
+                                        checkBalance.AccountBalance(accountNumber);
+                                        break;
+                                    case 3:
+                                        Console.Write("\nEnter the destination account number: ");
+                                        ulong toAccount = ulong.Parse(Console.ReadLine());
+                                        atm.DisplayMaskInfo(toAccount);
+                                        Console.Write("\nEnter amount to transfer: ");
+                                        decimal transferAmount = decimal.Parse(Console.ReadLine());
+                                        transfer.Transfer(accountNumber, toAccount, transferAmount);
+                                        break;
+                                    case 4:
+                                        history.DisplayHistory(accountNumber);
+                                        break;
+                                    case 5:
+                                        Console.WriteLine("\nExiting...\n");
+                                        return;
+                                    default:
+                                        Console.WriteLine("\nInvalid operation selected.");
+                                        break;
+                                }
+
+                                Console.Write("\n\nWant to transact again?  (yes or no)\n");
+                                Console.Write("\n> ");
+                                string response = Console.ReadLine().ToLower();
+
+                                if (response == "no")
+                                {
+                                    operations = false;
+                                }
+                                else if (response != "yes")
+                                {
+                                    throw new InvalidOperationException("\nInvalid input. Please enter 'yes' or 'no' only!\n");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nIncorrect pin code!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nAccount number not found!");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine($"\nWrong account number!\n");
+                }
+                Console.WriteLine();
+            }
+        }
         Console.ReadKey();
     }
 
